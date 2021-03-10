48b0273b42a78615b1621c8cfcb443b53845fbbe
Chris Wren <dazedpandas@gmail.com>
2021-03-10 15:13:08 +0000

    pick random quote from array using Math functions

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-// Get Quotes From API
 let apiQuotes = [];
 
+//Show New Quote
+function newQuote() {
+  //Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+
+// Get Quotes From API
 async function getQuotes() {
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
-    console.log(apiQuotes[10]);
+    newQuote();
   } catch (e) {
     //Catch Error Here
     alert(e);
