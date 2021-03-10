e905faa2b481fb203e5c8c6108f6cb01ce5bce24
Chris Wren <dazedpandas@gmail.com>
2021-03-10 15:31:16 +0000

    Replace empty author with Unknown and change font size for long quotes

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterButton = document.getElementById("twitter");
+const newQuoteButton = document.getElementById("new-quote");
+
 let apiQuotes = [];
 
 //Show New Quote
@@ -5,6 +11,19 @@ function newQuote() {
   //Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   console.log(quote);
+  //Check if Author field is blank and replace with 'Unknown'
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  //Check Quote length to determine font size
+  if (quote.text.length > 100) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API
